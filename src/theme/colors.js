693ce9ff925c1ef47a09e12d0c41307867283b5f
julian-jeong <julian-jeong@github.com>
2021-09-30 21:21:37 +0900

    Feat: add themes

---
+++ src/theme/colors.js
@@ -0,0 +1,18 @@
+const MainColors = {
+  navyBlue: '#005ac6',
+  orangeRed: '#fa6d33',
+  selectiveYellow: '#ffb200',
+  lightSalmon: '#ffa09d',
+  cosmic: '#713a67',
+  surfieGreen: '#017676',
+};
+
+const grayColors = {
+  white: '#FFFFFF',
+  lightGray: '#C5C5C5',
+  mediumGray: '#808080',
+  darkGray: '#3B3B3B',
+  black: '#000000',
+};
+
+export { MainColors, grayColors };
---
+++ src/theme/index.js
@@ -0,0 +1,20 @@
+import { MainColors, grayColors } from './colors';
+
+const buttonColors = {
+  main: MainColors.navyBlue,
+};
+
+const background = {
+  main: grayColors.white,
+  modal: grayColors.mediumGray,
+  innerModal: grayColors.lightGray,
+  input: grayColors.white,
+  disable: grayColors.mediumGray,
+};
+
+const theme = {
+  buttonColors,
+  background,
+};
+
+export default theme;
