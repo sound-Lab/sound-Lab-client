fa7369fdeafc2f5456af3f0585ec5098588b1b40
julian-jeong <julian-jeong@github.com>
2021-10-12 17:00:32 +0900

    Refactor: move music start function to MixEditor Header

--- src/containers/MixEditorContainer.jsx
+++ src/containers/MixEditorContainer.jsx
@@ -2,23 +2,19 @@ import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
-import * as Tone from 'tone';
 
 import { getMusicData } from '../modules/mixEditor';
-import { getInstrumentData, loadSoundData } from '../modules/instrument';
 
 import MixEditorHeader from '../components/MixEditorHeader';
 import TrackList from '../components/TrackList';
 import Sequencer from '../components/Sequencer';
+import SheetMusic from '../components/SheetMusic';
 import Loading from '../components/common/Loading';
 
-import { toneSampler } from '../lib/toneSampler';
-
 function MixEditorContainer() {
-  const { title, currentInstrument } = useSelector((state) => state.mixEditor);
-  const { instrument } = useSelector((state) => state.instrument);
+  const { title, tracks } = useSelector((state) => state.mixEditor);
   const { isLoading } = useSelector((state) => state.loading);
-  const [isSamplerloaded, setIsSamplerLoaded] = useState(false);
+  const [isSampleLoading, setIsSampleLoading] = useState(false);
   const { musicId } = useParams();
   const dispatch = useDispatch();
 
@@ -27,39 +23,12 @@ function MixEditorContainer() {
   }, [musicId]);
 
   useEffect(() => {
-    if (!currentInstrument) {
+    if (!tracks.length) {
       return;
     }
 
-    dispatch(getInstrumentData(currentInstrument));
-  }, [currentInstrument]);
-
-  useEffect(async () => {
-    if (isLoading === null && isLoading) {
-      return;
-    }
-
-    if (!currentInstrument) {
-      return;
-    }
-
-    setIsSamplerLoaded(false);
-
-    try {
-      const loadedInstrument = {};
-      const sampler = toneSampler(instrument[currentInstrument]);
-
-      loadedInstrument[currentInstrument] = sampler;
-
-      await Tone.loaded().then(() => {
-        dispatch(loadSoundData(loadedInstrument));
-      });
-    } catch (err) {
-      console.log(err);
-    }
-
-    return setIsSamplerLoaded((bool) => !bool);
-  }, [currentInstrument, isLoading]);
+    setIsSampleLoading(true);
+  }, [tracks.length]);
 
   return (
     <>
@@ -67,12 +36,9 @@ function MixEditorContainer() {
       <MixEditorWrapper>
         <TrackWrapper>
           <TrackList />
+          {isSampleLoading && <SheetMusic />}
         </TrackWrapper>
-        <ToolWrapper>
-          {!isLoading && isSamplerloaded && (
-            <Sequencer instrument={instrument[currentInstrument]} />
-          )}
-        </ToolWrapper>
+        <ToolWrapper>{isSampleLoading && <Sequencer />}</ToolWrapper>
         {isLoading && <Loading />}
       </MixEditorWrapper>
     </>
@@ -86,8 +52,7 @@ const MixEditorWrapper = styled.section`
 `;
 
 const TrackWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 20% 80%;
+  display: flex;
   background-color: ${({ theme }) => theme.MainColors.navyBlue};
 `;
 
@@ -97,4 +62,4 @@ const ToolWrapper = styled.div`
   background-color: green;
 `;
 
-export default React.memo(MixEditorContainer);
+export default MixEditorContainer;
