ea1036b0d1efbc3319c1578acb79a26eb3574512
julian-jeong <julian-jeong@github.com>
2021-10-12 17:04:39 +0900

    Feat: add sound sample load, track setting function

--- src/components/TrackList.jsx
+++ src/components/TrackList.jsx
@@ -1,49 +1,80 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
+import * as Tone from 'tone';
 
 import { ModalContext } from '../context/ModalContext';
-import { setInstrument } from '../modules/mixEditor';
+import { setTrack, setCurrentTrack } from '../modules/mixEditor';
+import { getInstrumentData } from '../modules/instrument';
+import { toneSampler, initialSteps } from '../lib/toneSampler';
 
 import ToolSetModal from './ToolSetModal';
 
 function TrackList() {
-  const [trackData, setTrackData] = useState(null);
+  const [selectedInstrument, setSelectedInstrument] = useState(null);
+  const { instrument } = useSelector((state) => state.instrument);
   const { tracks } = useSelector((state) => state.mixEditor);
   const { handleModal } = useContext(ModalContext);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (!trackData) {
+    if (!selectedInstrument) {
       return;
     }
 
-    dispatch(setInstrument(trackData));
     handleModal(null);
-  }, [trackData]);
+
+    if (instrument[selectedInstrument]) {
+      return;
+    }
+
+    dispatch(getInstrumentData(selectedInstrument));
+
+    return () => {
+      handleModal();
+    };
+  }, [selectedInstrument]);
+
+  useEffect(async () => {
+    if (!instrument[selectedInstrument]) {
+      return;
+    }
+    const steps = initialSteps(instrument[selectedInstrument]);
+    const sampler = toneSampler(instrument[selectedInstrument]);
+    const track = { name: selectedInstrument, sampler, steps };
+
+    try {
+      await Tone.loaded();
+    } catch (error) {
+      console.log(error);
+    }
+
+    dispatch(setTrack(track));
+    dispatch(setCurrentTrack(tracks.length));
+  }, [instrument[selectedInstrument], selectedInstrument]);
 
   function openTrackModal() {
-    handleModal(<ToolSetModal onSubmit={setTrackData} />);
+    handleModal(<ToolSetModal onSubmit={setSelectedInstrument} />);
   }
 
   return (
     <Wrapper>
       <button onClick={openTrackModal}>+ Add Track</button>
       {tracks.map((tool, index) => {
-        return <Track key={index}>{tool.title}</Track>;
+        return <Track key={index}>{tool.name}</Track>;
       })}
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
-  width: 100%;
+  width: 20%;
   height: 100%;
   background-color: ${({ theme }) => theme.grayColors.white};
 
   button {
     width: 100%;
-    height: 40px;
+    height: 15%;
     font-size: medium;
 
     &:hover {
