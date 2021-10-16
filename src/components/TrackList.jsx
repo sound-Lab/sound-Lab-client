7e1ecdb16afdb6fd232bad6f204889e7583c1874
julian-jeong <julian-jeong@github.com>
2021-10-17 02:49:31 +0900

    Feat: track sound handler

--- src/components/TrackList.jsx
+++ src/components/TrackList.jsx
@@ -4,16 +4,24 @@ import styled from 'styled-components';
 import * as Tone from 'tone';
 
 import { ModalContext } from '../context/ModalContext';
-import { setTrack, setCurrentTrack } from '../modules/mixEditor';
+import {
+  setTrack,
+  setCurrentTrack,
+  deleteCurrentTrack,
+  updateTrackMute,
+  updateTrackSolo,
+} from '../modules/mixEditor';
 import { getInstrumentData } from '../modules/instrument';
 import { toneSampler, initialSteps } from '../lib/toneSampler';
 
 import ToolSetModal from './ToolSetModal';
+import Loading from './common/Loading';
 
 function TrackList() {
   const [selectedInstrument, setSelectedInstrument] = useState(null);
+  const { tracks, currentTrack } = useSelector((state) => state.mixEditor);
   const { instrument } = useSelector((state) => state.instrument);
-  const { tracks } = useSelector((state) => state.mixEditor);
+  const { isLoading } = useSelector((state) => state.loading);
   const { handleModal } = useContext(ModalContext);
   const dispatch = useDispatch();
 
@@ -22,26 +30,33 @@ function TrackList() {
       return;
     }
 
-    handleModal(null);
-
     if (instrument[selectedInstrument]) {
+      handleModal(null);
       return;
     }
 
+    handleModal(null);
     dispatch(getInstrumentData(selectedInstrument));
-
-    return () => {
-      handleModal();
-    };
-  }, [selectedInstrument]);
+  }, [instrument, selectedInstrument]);
 
   useEffect(async () => {
     if (!instrument[selectedInstrument]) {
       return;
     }
-    const steps = initialSteps(instrument[selectedInstrument]);
+
+    const { codeName, midiSteps, stepsMap } = initialSteps(
+      instrument[selectedInstrument],
+    );
+
     const sampler = toneSampler(instrument[selectedInstrument]);
-    const track = { name: selectedInstrument, sampler, steps };
+
+    const track = {
+      name: selectedInstrument,
+      codeName,
+      midiSteps,
+      stepsMap,
+      mute: false,
+    };
 
     try {
       await Tone.loaded();
@@ -49,49 +64,169 @@ function TrackList() {
       console.log(error);
     }
 
-    dispatch(setTrack(track));
+    dispatch(setTrack({ track, sampler }));
     dispatch(setCurrentTrack(tracks.length));
-  }, [instrument[selectedInstrument], selectedInstrument]);
+  }, [instrument[selectedInstrument]]);
+
+  useEffect(() => {
+    setSelectedInstrument(null);
+  }, [tracks]);
 
   function openTrackModal() {
     handleModal(<ToolSetModal onSubmit={setSelectedInstrument} />);
   }
 
+  function handleTrack(ev) {
+    const targetTrack = ev.target.id;
+    const type = targetTrack.split('-');
+    const trackIndex = type[1];
+
+    switch (type[0]) {
+      case 'currentTrack':
+        dispatch(setCurrentTrack(trackIndex));
+        break;
+
+      case 'mute':
+        dispatch(updateTrackMute(trackIndex));
+        break;
+
+      case 'solo':
+        dispatch(updateTrackSolo(trackIndex));
+        break;
+
+      case 'delete':
+        dispatch(deleteCurrentTrack(trackIndex));
+        break;
+
+      default:
+        break;
+    }
+  }
+
   return (
     <Wrapper>
-      <button onClick={openTrackModal}>+ Add Track</button>
-      {tracks.map((tool, index) => {
-        return <Track key={index}>{tool.name}</Track>;
-      })}
+      <AddTrack onClick={openTrackModal}>+ Add Track</AddTrack>
+      <TrackListWrapper>
+        {tracks.map((tool, trackIndex) => {
+          return (
+            <Track key={trackIndex}>
+              <div className="effect">
+                <div
+                  className="mute"
+                  id={`mute-${trackIndex}`}
+                  onClick={handleTrack}>
+                  M
+                </div>
+                <div
+                  className="mute"
+                  id={`solo-${trackIndex}`}
+                  onClick={handleTrack}>
+                  S
+                </div>
+              </div>
+              <div
+                className="tool-name"
+                id={`currentTrack-${trackIndex}`}
+                onClick={handleTrack}>
+                {tool.name}
+              </div>
+              <div
+                className="delete"
+                id={`delete-${trackIndex}`}
+                onClick={handleTrack}>
+                x
+              </div>
+            </Track>
+          );
+        })}
+      </TrackListWrapper>
+      {isLoading && <Loading />}
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
-  width: 20%;
+  z-index: 1;
+  width: 200px;
   height: 100%;
   background-color: ${({ theme }) => theme.grayColors.white};
+  box-shadow: 5px 0px 16px 0px #0c0c0c3b;
+`;
 
-  button {
-    width: 100%;
-    height: 15%;
-    font-size: medium;
+const AddTrack = styled.div`
+  width: 100%;
+  height: 12%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: solid 0.5px #4444446c;
+  font-weight: 500;
+  box-shadow: -1px 2px 10px 0px #0c0c0c3b;
+  transition: background 0.3s;
 
-    &:hover {
-      background-color: ${({ theme }) => theme.MainColors.selectiveYellow};
-      color: ${({ theme }) => theme.grayColors.white};
-    }
+  &:hover {
+    background-color: ${({ theme }) => theme.mainColor.surfieGreen};
+    color: ${({ theme }) => theme.grayColors.white};
+  }
+
+  &:focus {
+    background: #393943;
   }
 `;
 
-const Track = styled.li`
-  height: 40px;
-  margin: auto;
-  background-color: ${({ theme }) => theme.grayColors.lightGray};
+const TrackListWrapper = styled.div`
+  width: 100%;
+  height: calc(100% - 12%);
+  overflow: scroll;
+`;
 
-  &:hover {
-    background-color: ${({ theme }) => theme.MainColors.surfieGreen};
-    color: ${({ theme }) => theme.grayColors.white};
+const Track = styled.div`
+  width: 100%;
+  height: 20%;
+  display: flex;
+  background-color: #f3f3f399;
+  border-bottom: solid 0.5px #41414199;
+  overflow: scroll;
+
+  .currentTrack {
+    width: 200%;
+    border-left: solid 0.5px #ff00004b;
+  }
+
+  .effect {
+    width: 30px;
+    height: 100%;
+    border-right: solid 0.5px #4141414c;
+  }
+
+  .mute {
+    width: 100%;
+    height: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: background 0.3s, color 0.3s;
+
+    &:hover {
+      color: white;
+      background: ${({ theme }) => theme.mainColor.selectiveYellow};
+    }
+  }
+
+  .tool-name {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .delete {
+    width: 20%;
+    height: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 `;
 
