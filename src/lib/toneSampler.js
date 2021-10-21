81d4988e6387e3412243c882168c38a296511b8c
julian-jeong <julian-jeong@github.com>
2021-10-21 23:58:43 +0900

    Feat: add sound

--- src/lib/toneSampler.js
+++ src/lib/toneSampler.js
@@ -25,12 +25,22 @@ export function initialSteps(instrument) {
   const tracks = {};
   const { codes } = instrument[0];
 
-  tracks.codeName = codes;
-  tracks.midiSteps = Array(16).fill(0);
-  tracks.stepsMap = codes.map((code) => ({
-    name: code,
-    steps: Array(64).fill(0),
-  }));
+  if (instrument[0].name === 'piano') {
+    const pianoCode = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4'];
+    tracks.codeName = pianoCode;
+    tracks.midiSteps = Array(16).fill(0);
+    tracks.stepsMap = codes.map((code) => ({
+      name: code,
+      steps: Array(64).fill(0),
+    }));
+  } else {
+    tracks.codeName = codes;
+    tracks.midiSteps = Array(16).fill(0);
+    tracks.stepsMap = codes.map((code) => ({
+      name: code,
+      steps: Array(64).fill(0),
+    }));
+  }
 
   return tracks;
 }
